e4d83022f6f6f2fd16088085a5f3117c113e0e50
MaximB0nd <sersim11@yandex.ru>
2025-07-10 00:05:35 +0300

    Deleted conviniens of contain

--- Sources/FuzzySearch/Array+FuzzySearchable.swift
+++ Sources/FuzzySearch/Array+FuzzySearchable.swift
@@ -13,18 +13,32 @@ public extension Array where Element: FuzzySearchable {
         
         var results: [(Element, Int)] = []
         
-        for element in self {
-            let target = element.searchableName
-            if abs(input.count - target.count) > maxWeightDistance {
-                continue
-            }
-            
-            let distance = input.levenshteinDistance(to: target)
-            if distance <= maxWeightDistance && element.searchableName.contains(input){
-                results.append((element, distance))
+        if input.count > 1 {
+            for element in self {
+                let target = element.searchableName
+                if abs(input.count - target.count) > maxWeightDistance {
+                    continue
+                }
+                
+                let distance = input.levenshteinDistance(to: target)
+                if distance <= maxWeightDistance {
+                    results.append((element, distance))
+                }
             }
         }
         
+        guard !results.isEmpty else {
+            for element in self {
+                if element.searchableName.contains(input){
+                    results.append((element, 0))
+                }
+            }
+            
+            print("No res in fuzzySearch")
+            return results.sorted { $0.0.searchableName.range(of: input)!.lowerBound < $1.0.searchableName.range(of: input)!.lowerBound }.map(\.0)
+        }
+        
+        print("Exist res in fuzzySearch")
         results.sort { $0.1 < $1.1 }
         return results.map { $0.0 }
     }
