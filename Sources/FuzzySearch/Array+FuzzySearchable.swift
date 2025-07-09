6a79ed209bc16137cfc5275c0780c56c070410a8
MaximB0nd <sersim11@yandex.ru>
2025-07-10 00:59:00 +0300

    Add sort to contains

--- Sources/FuzzySearch/Array+FuzzySearchable.swift
+++ Sources/FuzzySearch/Array+FuzzySearchable.swift
@@ -8,25 +8,28 @@
 import Foundation
 
 public extension Array where Element: FuzzySearchable {
-    func fuzzySearch(input: String, maxWeightDistance: Int = 5, minLength: Int = 3) -> [Element] {
+    func fuzzySearch(input: String, maxWeightDistance: Int = 5) -> [Element] {
         guard !input.isEmpty else { return [] }
         
         var results: [(Element, Int)] = []
         
-        if input.count > minLength {
-            for element in self {
-                let target = element.searchableName
-                if abs(input.count - target.count) > maxWeightDistance {
-                    continue
-                }
-                
-                let distance = input.levenshteinDistance(to: target)
-                if distance <= maxWeightDistance && distance != element.searchableName.count {
-                    print(element.searchableName, distance)
+        for element in self {
+            let target = element.searchableName
+            if abs(input.count - target.count) > maxWeightDistance {
+                continue
+            }
+            
+            let distance = input.levenshteinDistance(to: target)
+            if distance <= maxWeightDistance {
+                print(element.searchableName, distance)
+                if element.searchableName.contains(input) {
+                    results.append((element, distance/2))
+                } else {
                     results.append((element, distance))
                 }
             }
         }
+        
         
         results.sort { $0.1 < $1.1 }
         return results.map { $0.0 }
